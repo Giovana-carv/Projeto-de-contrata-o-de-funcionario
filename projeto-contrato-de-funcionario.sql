ee3b0a457fee7f0d65ffa75e81bc5745ce13c175
Giovana-carv <giovanacarvalho7002@gmail.com>
2025-04-22 11:29:10 -0300

    O banco de dados

---
+++ projeto-contrato-de-funcionario.sql
@@ -0,0 +1,46 @@
+USE project_servico;
+
+CREATE TABLE cnpj (
+    IDcnpj_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cnpj VARCHAR(100)
+);
+
+CREATE TABLE pessoa (
+    IDpessoa_PK INT PRIMARY KEY AUTO_INCREMENT,
+    CPF VARCHAR(11) UNIQUE,
+    nome VARCHAR(20),
+    IDcnpj_FK INT,
+    FOREIGN KEY (IDcnpj_FK) REFERENCES cnpj(IDcnpj_PK)
+);
+
+CREATE TABLE endereco (
+    IDendereco_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_endereco VARCHAR(100)
+);
+
+CREATE TABLE gerente (
+    IDgerente_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_gerente VARCHAR(50),
+    senha VARCHAR(100)
+);
+
+CREATE TABLE cliente (
+    IDcliente_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cliente VARCHAR(50),
+    IDendereco_FK INT,
+    IDpessoa_FK INT,
+    IDgerente_FK INT,
+    FOREIGN KEY (IDendereco_FK) REFERENCES endereco(IDendereco_PK),
+    FOREIGN KEY (IDpessoa_FK) REFERENCES pessoa(IDpessoa_PK),
+    FOREIGN KEY (IDgerente_FK) REFERENCES gerente(IDgerente_PK)
+);
+
+CREATE TABLE funcionario (
+    IDfuncionario_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_funcionario VARCHAR(50),
+    categoria VARCHAR(20),
+    IDpessoa_FK INT,
+    IDgerente_FK INT,
+    FOREIGN KEY (IDpessoa_FK) REFERENCES pessoa(IDpessoa_PK),
+    FOREIGN KEY (IDgerente_FK) REFERENCES gerente(IDgerente_PK)
+);
