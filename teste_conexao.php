6e175807e19b4e5a8ec5331c717fd0902564574b
Giovana-carv <giovanacarvalho7002@gmail.com>
2025-05-06 10:01:37 -0300

    teste de conexao

--- teste_conexao.php
+++ teste_conexao.php
@@ -1,20 +1,9 @@
 <?php
-$servername = "localhost"; // ou o endereço do seu servidor MySQL
-$username = "root"; // seu nome de usuário do banco de dados
-$password = ""; // sua senha do banco de dados
-$database = "project_servico"; // o nome do seu banco de dados
-
-// Cria a conexão
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verifica a conexão
-if ($conn->connect_error) {
-  die("Falha na conexão: " . $conn->connect_error);
-}
-echo "Conexão estabelecida com sucesso!";
-
-// Aqui você pode realizar operações no banco de dados
-
-// Fecha a conexão quando terminar
-$conn->close();
-?>
+$host="localhost";
+$user="root";
+$pass="";
+$banco="project_servico";
+$conexao=mysqli_connect($host, $user, $pass,
+    $banco) or die(mysql_error());
+    mysqli_select_db($conexao, $banco) or die(mysql_error());
+    ?>
