74a33031d8f31244081658d5b37497b7b78b49af
Giovana-carv <giovanacarvalho7002@gmail.com>
2025-06-10 08:59:13 -0300

    SQL

--- SLQ/projeto-contrato-de-funcionario.sql
+++ SLQ/projeto-contrato-de-funcionario.sql
@@ -5,43 +5,41 @@ CREATE TABLE pessoa (
     IDpessoa_PK INT PRIMARY KEY AUTO_INCREMENT,
     senha_pessoa VARCHAR(100),
     CPF VARCHAR(11) UNIQUE,
-    nome VARCHAR(20),
+    nome VARCHAR(20)
+    );
 
-    
+CREATE TABLE cliente (
+    IDcliente_PK INT PRIMARY KEY AUTO_INCREMENT,
+    nome_cliente VARCHAR(50),
+    email_cliente VARCHAR(50),
+    senha_cliente VARCHAR(100),
+    IDpessoa INT,
+    FOREIGN KEY (IDpessoa) REFERENCES pessoa(IDpessoa_PK)
 );
+
 
 CREATE TABLE endereco (
     IDendereco_PK INT PRIMARY KEY AUTO_INCREMENT,
     IDcliente_FK INT,
     nome_endereco_cliente VARCHAR(100),
-    nome_endereco_cnpj VARCHAR(100)
+    nome_endereco_cnpj VARCHAR(100),
     FOREIGN KEY (IDcliente_FK) REFERENCES cliente(IDcliente_PK)
 );
 
 CREATE TABLE gerente (
     IDgerente_PK INT PRIMARY KEY AUTO_INCREMENT,
-    IDcadger INT UNIQUE,
+    cnpj INT,
     senha_gerente VARCHAR(100),
     nome_gerente VARCHAR(50)
 );
 
-CREATE TABLE cliente (
-    IDcliente_PK INT PRIMARY KEY AUTO_INCREMENT,
-    nome_cliente VARCHAR(50),
-    email_cliente VARCHAR(50),,
-    senha_cliente VARCHAR(100),
-    CPF_pessoa INT,
-    FOREIGN KEY (CPF_pessoa) REFERENCES pessoa(CPF)
-);
-
 CREATE TABLE funcionario (
     IDfuncionario_PK INT PRIMARY KEY AUTO_INCREMENT,
-    IDcadfun INT UNIQUE,
     senha_funcionario VARCHAR(100),
     nome_funcionario VARCHAR(50),
     categoria VARCHAR(20),
-    IDpessoa_FK INT,
-    FOREIGN KEY (IDpessoa_FK) REFERENCES pessoa(IDpessoa_PK)
+    IDpessoa INT,
+    FOREIGN KEY (IDpessoa) REFERENCES pessoa(IDpessoa_PK)
   
 );
 
